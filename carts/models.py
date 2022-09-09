85b34310f829adec6609266ee0530f834c70ed88
hamza.stahi <hamza.stahi@orange.com>
2022-09-10 00:52:19 +0200

    variations

--- carts/models.py
+++ carts/models.py
@@ -15,7 +15,7 @@ class CartItem(models.Model):
   product = models.ForeignKey(Product, on_delete=models.CASCADE)
   cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
   quantity = models.IntegerField()
-  variation = models.ManyToManyField(Variation, blank=True)
+  variations = models.ManyToManyField(Variation, blank=True)
 
   def __str__(self):
     return self.product.product_name
